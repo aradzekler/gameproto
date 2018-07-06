f24b67ce8c0dbbaba1df2d2cf158922e0674f69a
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-07-06 19:36:21 +0300

    Update Player.gd
    fixed double jump. added dash

--- gameproto1/Player.gd
+++ gameproto1/Player.gd
@@ -6,6 +6,7 @@ const MAX_SPEED = 200
 const ACCELERATION = 50
 const JUMP_HEIGHT = -550
 const MAX_JUMPS = 2
+const DASH_SPEED = 200
 const DASH_TIMER = 30
 const MAX_FALL_TIME = 100
 
@@ -21,17 +22,21 @@ var jumpCount = 0
 var deathFallCounter = 0
 var dashTimerCounter = 0
 var myDelta = 0
+
+# InputMap variables.
 var moveLeft = input_states.new("ui_left")
 var moveRight = input_states.new("ui_right")
 var moveUp = input_states.new("ui_select")
 var moveDown = input_states.new("ui_down")
+var Dash = input_states.new("ui_shift")
 
 ##TODO: [] Add duck.
-##		[] Fix double jump 
-func get_input():
+func _get_input():
 	var friction = false
 	var onFloor = is_on_floor() #boolean
 	
+	if onFloor:
+		jumpCount = 0
 	if moveRight.check() == INPUT_PRESSED:
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
@@ -40,24 +45,22 @@ func get_input():
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 		motion.x = clamp(motion.x - ACCELERATION, -MAX_SPEED, MAX_SPEED)
-	if moveDown.check() == INPUT_PRESSED and onFloor:
-		#$Sprite.play("Duck") future duck sprite.
-		deathFallCounter = 0
+	if Dash.check() == INPUT_PRESSED and moveRight.check() == INPUT_PRESSED and moveLeft.check() != INPUT_PRESSED:
+		motion.x = motion.x + DASH_SPEED 
+		friction = true
+	if Dash.check() == INPUT_PRESSED and moveLeft.check() == INPUT_PRESSED and moveRight.check() != INPUT_PRESSED:
+		motion.x = motion.x - DASH_SPEED
 		friction = true
 	if onFloor and moveRight.check() == INPUT_RELEASED and moveLeft.check() == INPUT_RELEASED:
 		$Sprite.play("Idle")
 		deathFallCounter = 0
 		jumpCount = 0
 		friction = true
-	if moveUp.check() == INPUT_JUST_PRESSED and onFloor: #jump
+	if moveUp.check() == INPUT_JUST_PRESSED and jumpCount < MAX_JUMPS: #jump
 		deathFallCounter = 0
 		$Sprite.play("Jump")
 		motion.y += JUMP_HEIGHT
 		jumpCount += 1
-	if moveUp.check() == INPUT_JUST_PRESSED and jumpCount < MAX_JUMPS and !onFloor: # double jump.
-		motion.y += JUMP_HEIGHT * 0.7 
-		jumpCount += 1
-		$Sprite.play("DoubleJump")
 	if !onFloor and motion.y > 0:
 		deathFallCounter += 1
 		$Sprite.play("Fall")
@@ -68,7 +71,7 @@ func get_input():
 		motion.x = lerp(motion.x, 0, 0.05) #less friction while moving through air.
 
 # resets scene after player falls too much
-func fall_killer():
+func _fall_killer():
 	if deathFallCounter > MAX_FALL_TIME:
 		    if !is_on_floor():
      			 get_tree().reload_current_scene()
@@ -81,8 +84,8 @@ func _fixed_process(delta):
 func _physics_process(delta):
 	motion.y += GRAVITY # basic gravity,  adds every frame.
 
-	get_input()
-	fall_killer()
+	_get_input()
+	_fall_killer()
 	motion = move_and_slide(motion, UP)	# making movement
 	pass
 
